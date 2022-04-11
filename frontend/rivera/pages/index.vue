f74dfd1ad843a0b278cb25a244d2d779a68b9ed9
andjmiskovic <andjelamiskovic2000@gmail.com>
2022-04-11 20:19:29 +0200

    #3.1.1 bugfix

--- frontend/rivera/pages/index.vue
+++ frontend/rivera/pages/index.vue
@@ -1,31 +1,12 @@
 <template>
-  <div class="container">
-    <navbar/>
-  </div>
+    <unauthenticated-navbar/>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue';
+import UnauthenticatedNavbar from '../components/UnauthenticatedNavbar.vue'
+
 export default {
-  components: { Navbar },
+  components: { UnauthenticatedNavbar  },
   name: 'IndexPage'
 }
 </script>
-
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap");
-body {
-  font-family: "Roboto", sans-serif;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-</style>
--- frontend/rivera/pages/trending.vue
+++ frontend/rivera/pages/trending.vue
@@ -1,14 +1,15 @@
 <template>
 <div>
-    <navbar/>
+    <unauthenticated-navbar/>
     <p>Trending</p>
 </div> 
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue';
+import UnauthenticatedNavbar from '../components/UnauthenticatedNavbar.vue'
 
 export default {
+  components: { UnauthenticatedNavbar },
 
 }
 </script>
