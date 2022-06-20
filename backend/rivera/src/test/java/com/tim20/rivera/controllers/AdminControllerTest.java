9dafaac2c6a2c3c5167e4f8a4304bcdd8e802855
Pavle <pavleg.19@gmail.com>
2022-06-21 01:49:59 +0200

    Fix tests

--- backend/rivera/src/test/java/com/tim20/rivera/controllers/AdminControllerTest.java
+++ backend/rivera/src/test/java/com/tim20/rivera/controllers/AdminControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,13 +32,14 @@ public class AdminControllerTest {
 
     @Before
     public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
     }
 
     @Test
     public void getRules() throws Exception {
         mockMvc
-                .perform(get("/api/admin/rules")).andExpect(status().isOk())
+                .perform(get("/api/admin/rules")
+                        .with(user("admin").password("sifra").roles("ADMIN"))).andExpect(status().isOk())
                 .andExpect(jsonPath("$.pointsPerReservation").value(5))
                 .andExpect(jsonPath("$.incomePercentage").value(50));
     }
@@ -44,7 +47,8 @@ public class AdminControllerTest {
     @Test
     public void getOwnerRequest() throws Exception {
         mockMvc
-                .perform(get("/api/admin/owner-request?username=marko"))
+                .perform(get("/api/admin/owner-request?username=marko")
+                        .with(user("admin").password("sifra").roles("ADMIN")))
                 .andExpect(jsonPath("$.username").value("marko"))
                 .andExpect(jsonPath("$.name").value("Mitar"))
                 .andExpect(jsonPath("$.surname").value("Markovic"))
@@ -54,7 +58,8 @@ public class AdminControllerTest {
     @Test
     public void getCategories() throws Exception {
         mockMvc
-                .perform(get("/api/admin/categories"))
+                .perform(get("/api/admin/categories")
+                        .with(user("admin").password("sifra").roles("ADMIN")))
                 .andExpect(jsonPath("$.[*].percentage").value(hasItem(5.0)))
                 .andExpect(jsonPath("$.[*].numberOfPoints").value(hasItem(0)))
                 .andExpect(jsonPath("$.[*].name").value(hasItem("Basic")));
@@ -65,7 +70,8 @@ public class AdminControllerTest {
     public void getPendingTerminationRequest() throws Exception {
         // (id, description, status, person_id, version) values (1, 'Opis', 2, 'pera', 0)
         mockMvc
-                .perform(get("/api/admin/pending-termination-requests"))
+                .perform(get("/api/admin/pending-termination-requests")
+                        .with(user("admin").password("sifra").roles("ADMIN")))
                 .andExpect(jsonPath("$.[*].username").value(hasItem("pera")))
                 .andExpect(jsonPath("$.[*].description").value(hasItem("Opis")))
                 .andExpect(jsonPath("$.[*].requestId").value(hasItem(1)));
@@ -92,7 +98,8 @@ VALUES ('2022-05-21 17:44:47.41532', 3.3, 1,
         private boolean complaint;
          */
         mockMvc
-                .perform(get("/api/admin/pending-reviews"))
+                .perform(get("/api/admin/pending-reviews")
+                        .with(user("admin").password("sifra").roles("ADMIN")))
                 .andExpect(jsonPath("$.[*].clientName").value(hasItem("Petar")))
                 .andExpect(jsonPath("$.[*].clientSurname").value(hasItem("Petrovic")))
                 .andExpect(jsonPath("$.[*].reviewedName").value(hasItem("Mountain Fishing")))
--- backend/rivera/src/test/java/com/tim20/rivera/controllers/OwnerControllerTest.java
+++ backend/rivera/src/test/java/com/tim20/rivera/controllers/OwnerControllerTest.java
@@ -1,35 +1,57 @@
 package com.tim20.rivera.controllers;
 
+import com.tim20.rivera.dto.JwtAuthenticationRequestDTO;
+import com.tim20.rivera.dto.UserTokenState;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Arrays;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OwnerControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Autowired
+    private TestRestTemplate template;
+
+
     @Before
     public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                                      .apply(springSecurity())
+                                      .build();
     }
 
     @Test
     public void getAttendance() throws Exception {
+
+
         mockMvc
-                .perform(get("/api/get-attendance?&startDate=2022-06-01&endDate=2022-06-30&type=week"))
+                .perform(get("/api/get-attendance?&startDate=2022-06-01&endDate=2022-06-30&type=week")
+                        .with(user("cowner").password("sifra").roles("COTTAGE_OWNER"))
+                )
                 .andExpect(jsonPath("$.[3].timestamp").value("2022-06-20T00:00:00"))
                 .andExpect(jsonPath("$.[3].count").value(1))
                 .andExpect(jsonPath("$.[2].timestamp").value("2022-06-13T00:00:00"))
